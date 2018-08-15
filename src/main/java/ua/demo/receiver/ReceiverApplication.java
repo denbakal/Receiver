bfb43aff2b7b49f1bed9d848fadd04991b751aef
Bakal <d.bakal@controlpay.com>
2018-08-15 18:32:14 +0300

    Simple queue

--- src/main/java/ua/demo/receiver/ReceiverApplication.java
+++ src/main/java/ua/demo/receiver/ReceiverApplication.java
@@ -12,9 +12,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @SpringBootApplication
 @EnableRabbit
 public class ReceiverApplication implements RabbitListenerConfigurer {
@@ -22,10 +19,6 @@ public class ReceiverApplication implements RabbitListenerConfigurer {
 	public SimpleRabbitListenerContainerFactory myRabbitListenerContainerFactory() {
 		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
 		factory.setConnectionFactory(connectionFactory());
-		factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
-//		factory.setConcurrentConsumers(5);
-//		factory.setMaxConcurrentConsumers(10);
-//		factory.setPrefetchCount(5);
 		return factory;
 	}
 
@@ -39,19 +32,6 @@ public class ReceiverApplication implements RabbitListenerConfigurer {
 		return new RabbitAdmin(connectionFactory());
 	}
 
-	/*@Bean
-	public RabbitTemplate rabbitTemplate() {
-		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-		rabbitTemplate.setQueue("report_queue");
-//		rabbitTemplate.setReplyTimeout(60 * 100); //no reply to - we use direct-reply-to
-		return rabbitTemplate;
-	}*/
-
-//	@Bean
-//	public Queue reportQueue() {
-//		return new Queue("report_queue");
-//	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReceiverApplication.class, args);
 	}
@@ -61,15 +41,6 @@ public class ReceiverApplication implements RabbitListenerConfigurer {
 		registrar.setContainerFactory(myRabbitListenerContainerFactory());
 	}
 
-	/* DLX */
-	/*@Bean
-	public Queue retryQueue() {
-		Map<String, Object> args = new HashMap<>();
-		args.put("x-dead-letter-exchange", "dlx");
-		args.put("x-message-ttl", 5000);
-		return new Queue("report_queue", false, false, false, args);
-	}*/
-
 	@Bean
 	public DirectExchange reportExchange() {
 		return new DirectExchange("report_exchange");
@@ -77,11 +48,7 @@ public class ReceiverApplication implements RabbitListenerConfigurer {
 
 	@Bean
 	public Queue reportQueue() {
-		Map<String, Object> args = new HashMap<>();
-		args.put("x-dead-letter-exchange", "retry_exchange");
-		args.put("x-dead-letter-routing-key", "retry_queue_key");
-//		args.put("x-message-ttl", 5000);
-		return new Queue("report_queue", true, false, false, args);
+		return new Queue("report_queue", true);
 	}
 
 	@Bean
@@ -89,28 +56,5 @@ public class ReceiverApplication implements RabbitListenerConfigurer {
 		return BindingBuilder.bind(reportQueue())
 				.to(reportExchange())
 				.with("report_queue_key");
-	}
-
-	@Bean
-	public DirectExchange retryExchange() {
-		return new DirectExchange("retry_exchange");
-	}
-
-	@Bean
-	public Queue retryQueue() {
-		Map<String, Object> args = new HashMap<>();
-		args.put("x-dead-letter-exchange", "report_exchange");
-		// Route to the incoming queue when the TTL occurs
-		args.put("x-dead-letter-routing-key", "report_queue_key");
-		// TTL 15 seconds
-		args.put("x-message-ttl", 15000);
-		return new Queue("retry_queue", true, false, false, args);
-	}
-
-	@Bean
-	public Binding bindingRetry() {
-		return BindingBuilder.bind(retryQueue())
-				.to(retryExchange())
-				.with("retry_queue_key");
 	}
 }
--- src/main/java/ua/demo/receiver/listener/Receiver.java
+++ src/main/java/ua/demo/receiver/listener/Receiver.java
@@ -1,13 +1,9 @@
 package ua.demo.receiver.listener;
 
-import com.rabbitmq.client.Channel;
 import lombok.extern.log4j.Log4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Log4j
@@ -16,31 +12,7 @@ public class Receiver {
     private AtomicInteger count = new AtomicInteger(0);
 
     @RabbitListener(queues = "report_queue")
-    public void worker(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws InterruptedException, IOException {
+    public void worker(String message) {
         log.info("Received on worker : " + message + ", count: " + count.incrementAndGet());
-        Thread.sleep(2000L);
-
-//        channel.basicAck(tag, false);
-//        channel.basicReject(tag, true);
-        channel.basicReject(tag, false);
-
-        /*if (count.get() == 20) {
-            channel.basicAck(tag, false);
-            log.info("Sending >>> basicNack " + tag);
-            Receiver.isACK = true;
-        }
-
-        if (Receiver.isACK) {
-            channel.basicAck(tag, false);
-            log.info("Sending >>> basicNack " + tag);
-        } else {
-            channel.basicReject(tag, true);
-        }*/
     }
-
-    /*@RabbitListener(queues = "report_queue")
-    public void worker(String message) throws InterruptedException, IOException {
-        log.info("Received on worker : " + message);
-        log.info("Sending >>> basicNack ");
-    }*/
 }
